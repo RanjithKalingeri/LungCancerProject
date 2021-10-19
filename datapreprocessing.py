9f956e0085d757a3a4803fb6cff8cb84990fd70d
Ranjith Kalingeri <76031174+RanjithKalingeri@users.noreply.github.com>
2021-10-19 23:14:10 +0530

    Add files via upload

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -8,8 +8,8 @@ mydevice = "cuda" if torch.cuda.is_available() else "cpu"
 folder_path = r"C:\Users\ranji\Desktop\DeepLearning\manifest-cgqtDj7Y2699835271585651107\SPIE-AAPM Lung CT Challenge"
 all_images = os.listdir(folder_path)
 # print(all_images)
-i = 1
-for image in all_images[0:1]:
+k = 0
+for image in all_images:
     first_click = os.path.join(folder_path, image)
     first_click_dir = os.listdir(first_click)
     second_click = os.path.join(first_click, first_click_dir[0])
@@ -21,20 +21,26 @@ for image in all_images[0:1]:
     for dimage in third_click_dir:
         ds = dicom.dcmread(os.path.join(third_click, dimage))
         pixel_array_numpy = ds.pixel_array
-        pixel_array_numpy = pixel_array_numpy.astype("float64")
-        d_tensor = torch.from_numpy(pixel_array_numpy)
-        lis.append(d_tensor)
+        # image_hfu = [[0]*512 for i in range(512)]
+        updated_image = [[0] * 512 for i in range(512)]
+        for i in range(512):
+            for j in range(512):
+                image_hfu = pixel_array_numpy[i][j] - float(1024)
+                if image_hfu > float(-1000) and image_hfu < float(-300):
+                    updated_image[i][j] = pixel_array_numpy[i][j]/float(255)
+        image_tensor = torch.Tensor(updated_image)
+        image_tensor.to(mydevice)
+        lis.append(image_tensor)
     final3d_tensor = torch.stack(lis, dim=0)
-
-
+    final3d_tensor.to(mydevice)
     final3d_tensor = final3d_tensor.permute(2, 1, 0)
     final3d_tensor = nnf.interpolate(final3d_tensor, size=250)
     # final3d_tensor = final3d_tensor.permute(2, 1, 0)
+    file_name = r'C:\Users\ranji\Desktop\DeepLearning\patient3D_tensors'
+    image_name = 'final3dimage' + str(k)
+    final_path = os.path.join(file_name, image_name)
+    # print(final_path)
+    k = k+1
+    torch.save(final3d_tensor, final_path)
     print(final3d_tensor.shape)
-    # nnf.normalize(final3d_tensor, p=2, dim=1)
-    # print(final3d_tensor)
-    # mean, std = final3d_tensor.mean([1,2]), final3d_tensor.std([1,2])
-    # print(mean)
-    # print(std)
-    print("---------")
-    i = i+1
+    print(k)
